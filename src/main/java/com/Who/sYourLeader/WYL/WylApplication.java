42f1e9a4d117e87629b36cc0e369c56d2326aab0
Yash Goswami <yashgoswami2000@gmail.com>
2022-07-15 21:46:40 +0530

    Initial commit

---
+++ src/main/java/com/Who/sYourLeader/WYL/WylApplication.java
@@ -0,0 +1,13 @@
+package com.Who.sYourLeader.WYL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WylApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Who/sYourLeader/WYL/WylApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Who.sYourLeader.WYL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WylApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
