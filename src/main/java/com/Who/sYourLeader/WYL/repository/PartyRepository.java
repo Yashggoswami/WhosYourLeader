9c358a44d7fbb54da13545b31efd345158796176
= <manishchouhan5124@gmail.com>
2022-07-16 19:10:55 +0530

    added a method in PartyRepository

--- src/main/java/com/Who/sYourLeader/WYL/repository/PartyRepository.java
+++ src/main/java/com/Who/sYourLeader/WYL/repository/PartyRepository.java
@@ -1,8 +1,14 @@
 package com.Who.sYourLeader.WYL.repository;
 
 
+import com.Who.sYourLeader.WYL.entity.Candidate;
 import com.Who.sYourLeader.WYL.entity.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PartyRepository extends JpaRepository<Party, Long> {
+    public static List<Candidate> findCandidateByPartyId(long id) {
+        return null;
+    }
 }
