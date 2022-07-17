070c149c7f961f734f8d70c4e781dc1a70e9742d
Yash Goswami <yashgoswami2000@gmail.com>
2022-07-17 21:28:01 +0530

    frontend html get mapping done

--- src/main/java/com/Who/sYourLeader/WYL/controller/CandidateController.java
+++ src/main/java/com/Who/sYourLeader/WYL/controller/CandidateController.java
@@ -2,20 +2,13 @@ package com.Who.sYourLeader.WYL.controller;
 
 import com.Who.sYourLeader.WYL.dto.CandidateDto;
 import com.Who.sYourLeader.WYL.service.CandidateService;
-import com.Who.sYourLeader.WYL.service.impl.CandidateServiceImpl;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @Controller
 public class CandidateController{
-
-
 
     public CandidateController(CandidateService candidateService) {
         this.candidateService = candidateService;
@@ -24,13 +17,13 @@ public class CandidateController{
     @GetMapping("/candidates")
     public String getCandidates(Model model){
         model.addAttribute("candidates",candidateService.getAllCandidate());
-        return "index";
+        return "home";
     }
 
-    @GetMapping("/allcandidates")
-    public List<CandidateDto> getAllCandidates(){
-        return candidateService.getAllCandidate();
-    }
+//    @GetMapping("/allcandidates")
+//    public List<CandidateDto> getAllCandidates(){
+//        return candidateService.getAllCandidate();
+//    }
 }
 
 
