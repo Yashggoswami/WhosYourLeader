244119a9c89bc3c8b64908dce8dc3044fe6f6a5c
= <manishchouhan5124@gmail.com>
2022-07-22 18:28:50 +0530

    service rearrangement completed

--- src/main/java/com/Who/sYourLeader/WYL/controller/CandidateController.java
+++ src/main/java/com/Who/sYourLeader/WYL/controller/CandidateController.java
@@ -9,8 +9,7 @@ import com.Who.sYourLeader.WYL.repository.ConstituencyRepository;
 import com.Who.sYourLeader.WYL.repository.ElectionRepository;
 import com.Who.sYourLeader.WYL.repository.ElectionTypeRepository;
 import com.Who.sYourLeader.WYL.repository.StatesRepository;
-import com.Who.sYourLeader.WYL.service.CandidateService;
-import com.Who.sYourLeader.WYL.service.ElectionService;
+import com.Who.sYourLeader.WYL.service.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -36,9 +35,22 @@ import java.util.Map;
 public class CandidateController{
 
 
-    private CandidateService candidateService;
+    @Autowired
     private ElectionService electionService;
-//    @GetMapping("/candidates")
+
+    @Autowired
+    private ElectionTypeService electionTypeService;
+
+    @Autowired
+    private StatesService statesService;
+
+    @Autowired
+    private ConstituencyService constituencyService;
+
+    @Autowired
+    private CandidateService candidateService;
+
+    //    @GetMapping("/candidates")
 //    public String getCandidates(Model model){
 //        model.addAttribute("elections",electionService.getAllElections());
 //        model.addAttribute("candidates",candidateService.getAllCandidate());
@@ -50,32 +62,40 @@ public class CandidateController{
 //        model.addAttribute("elections",electionService.getElectionType(id));
 //        return "home1";
 //    }
-    @Autowired
-    ElectionRepository electionRepository;
-    @Autowired
-    ElectionTypeRepository electionTypeRepository;
-
-    @Autowired
-    StatesRepository statesRepository;
-
-    @Autowired
-    ConstituencyRepository constituencyRepository;
 
     @GetMapping("/elections")
-    public List<Election> getElectionType(){
-        return electionRepository.findAll();
+    public List<Election> getAllElection(){
+        return electionService. getAllElections();
     }
-    @GetMapping("/electiontype/{id}")
-    public List<ElectionType> getElectionTypeByElectionId(@PathVariable Long id){
-        return electionTypeRepository.findElectionTypesByElectionsElectionId(id);
+
+
+    @GetMapping("/electionTypes")
+    public List<ElectionType> getAllElectionType()
+    {
+        return electionTypeService.getAllElectionTypes();
     }
+
+
+    @GetMapping("/electiontype/{electionid}")
+    public List<ElectionType> getElectionTypesByElectionId(@PathVariable Long electionid){
+        return electionTypeService.getElectionTypeByElectionId(electionid);
+    }
+
     @GetMapping("/states/{electionid}/{electiontypeid}")
-    public List<States> getElectionTypeByElectionId(@PathVariable("electionid") Long electionid, @PathVariable("electiontypeid") Long electiontypeid){
-        return statesRepository.findStatessByElectionsElectionIdAndElectionTypesElectiontypeId(electionid,electiontypeid);
+    public List<States> getStateByElectionIdAndElectionTypeId(@PathVariable("electionid") Long electionid, @PathVariable("electiontypeid") Long electiontypeid){
+        return statesService.getStatesByElectionIdAndElectionTypeId(electionid,electiontypeid);
     }
+
+
     @GetMapping("/constituency/{electiontypeid}/{stateid}")
     public List<Constituency> getConstituency( @PathVariable("electiontypeid") Long electiontypeid, @PathVariable("stateid") Long stateid){
-        return constituencyRepository.findConstituencysByElectiontypes_ElectiontypeIdAndState_StateId(electiontypeid,stateid);
+        return constituencyService.getConstituencyByElectionTypeIdAndStateId(electiontypeid, stateid);
+    }
+
+
+    @GetMapping("/candidates/{constituencyid}")
+    public List<CandidateDto> getCandidates(@PathVariable Long constituencyid){
+        return candidateService.getCandidates(constituencyid);
     }
 }
 
--- src/main/java/com/Who/sYourLeader/WYL/repository/CandidateRepository.java
+++ src/main/java/com/Who/sYourLeader/WYL/repository/CandidateRepository.java
@@ -3,5 +3,9 @@ package com.Who.sYourLeader.WYL.repository;
 import com.Who.sYourLeader.WYL.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+    List<Candidate> getCandidatesByConstituency_ConstituencyId(Long constituencyId);
 }
