e99b908eb4e0fb73b4bd05e75b502eef8ab0e0a6
Yash Goswami <yashgoswami2000@gmail.com>
2022-08-05 17:33:34 +0530

    spring registery added

---
+++ wyl-service-registery/src/main/java/com/example/wylserviceregistery/WylServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.wylserviceregistery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WylServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WylServiceRegisteryApplication.class, args);
+	}
+
+}
---
+++ wyl-service-registery/src/test/java/com/example/wylserviceregistery/WylServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wylserviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WylServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
