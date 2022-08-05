c6922376f0575a6f3f5a0f8de700712564761edf
vinaygoswami321 <vinaygoswami2001@gmail.com>
2022-08-05 18:59:35 +0530

    service-register connection done

--- WYL/src/main/java/com/Who/sYourLeader/WYL/WylApplication.java
+++ WYL/src/main/java/com/Who/sYourLeader/WYL/WylApplication.java
@@ -3,9 +3,11 @@ package com.Who.sYourLeader.WYL;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WylApplication {
 
 	@Bean
--- wyl-service-registery/src/main/java/com/example/wylserviceregistery/WylServiceRegisteryApplication.java
+++ wyl-service-registery/src/main/java/com/example/wylserviceregistery/WylServiceRegisteryApplication.java
@@ -2,8 +2,10 @@ package com.example.wylserviceregistery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WylServiceRegisteryApplication {
 
 	public static void main(String[] args) {
