066da35b3e2dbf531511a6340398119c6fde83f8
Yash Goswami <yashgoswami2000@gmail.com>
2022-08-11 14:13:27 +0530

    cloud config server added

---
+++ wyl-cloud-config-server/src/test/java/com/example/wylcloudconfigserver/WylCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wylcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WylCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
